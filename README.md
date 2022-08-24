42e022019170ae61916d5b7a86281d281dda2b4d
Rahul Noronha <rahulnor@gmail.com>
2022-08-24 18:13:32 +0530

    Added dialog box for folder input

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ To run:
 ***
 1. <b>Download script.py</b>
 1. <b>pip install MoviePy</b>
-1. <b>Enter the folder path to the folder where all your MPG files are when the command prompt asks for use input</b>
+1. <b>Enter the folder path to the folder where all your MPG files are when the command prompt asks for use input using dialog box</b>
 ```bash
 python script.py
 ```
--- script.py
+++ script.py
@@ -1,9 +1,11 @@
+import tkinter as tk
+from tkinter import filedialog
 import moviepy.editor as moviepy
 import os
 import os.path
-folder_path = input("Enter the path where your MPG files are")
+folder_path = filedialog.askdirectory()
 files = os.listdir(folder_path)
 for file in files:
     print(f"{folder_path}{file[0:-4]}.mp4")
-    clip = moviepy.VideoFileClip(f"{folder_path}{file[0:-4]}.MPG")
-    clip.write_videofile(f"{folder_path}{file[0:-4]}.mp4")
+    clip = moviepy.VideoFileClip(f"{folder_path}/{file[0:-4]}.MPG")
+    clip.write_videofile(f"{folder_path}/{file[0:-4]}.mp4")
